026cb1c4fa05e73bb3f4c334270dc95780eee09a
Sunil Boga <30490515+boga5@users.noreply.github.com>
2017-12-01 18:00:34 +0530

    Update robot.sh

--- robot/robot.sh
+++ robot/robot.sh
@@ -4,4 +4,8 @@ HOSTNAME=$1
 PORT=$2
 DBURL=$3
 
+echo "HOSTNAME=$1"
+echo "PORT=$2"
+echo "DBURL=$3"
+
 pybot --variable hostname:${HOSTNAME} --variable port:${PORT} --variable dbConnectionStr:${DBURL} tests/sampletest.robot
