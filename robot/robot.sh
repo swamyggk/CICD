5206f1a118896e8676823b43b39274a1d0137588
Sunil Boga <30490515+boga5@users.noreply.github.com>
2017-12-02 11:15:39 +0530

    Delete robot.sh

--- robot/robot.sh
+++
@@ -1,8 +0,0 @@
-echo "Execute robot framework: Connect to Oracle, test on sample project services"
-HOSTNAME=$1
-PORT=$2
-DBURL=$3
-#echo "HOSTNAME=$1"
-#echo "PORT=$2"
-#echo "DBURL=$3"
-pybot --variable hostname:${HOSTNAME} --variable port:${PORT} --variable dbConnectionStr:${DBURL} ./tests/sampletest.robot
