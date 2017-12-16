5064e00ecd761428d9f0423c3deab865649b6edf
SnehaKailasa <sneha.kailasa@ggktech.com>
2017-12-16 12:05:21 +0530

    Create wait_for_robot.sh

---
+++ wait_for_robot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source .env
+sleep 10s
+Container_status=`docker ps | grep "$robot_container_name"`
+#echo $Container_status
+while [ -n "$Container_status" ]
+do
+  echo sleeping;
+  sleep 5s;
+  Container_status=`docker ps | grep "$robot_container_name"`
+done;
